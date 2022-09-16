5028d5de029d275b5e5a905cac141e334caae719
ja50n <zs_feng@qq.com>
2022-09-16 21:46:57 +0800

    done 1.9.camera

--- apps/1.9.camera/main.cpp
+++ apps/1.9.camera/main.cpp
@@ -12,14 +12,81 @@
 constexpr int SCR_W = 800;
 constexpr int SCR_H = 600;
 
-void framebuffer_size_cb(GLFWwindow *window, int w, int h) {
+glm::vec3 cam_pos = glm::vec3(0.0f, 0.0f, 3.0f);
+// Front direction, not looking at point
+glm::vec3 cam_front = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cam_up = glm::vec3(0.0f, 1.0f, 0.0f);
+
+/**
+ * @brief Delta time is the time cost to render last frame.
+ * If it's big, we need to go faster in this frame, so
+ * delta_time is suitable for the time duration of current frame.
+ */
+float delta_time = 0;
+float last_frame = 0;
+/**
+ * @brief Last position of mouse
+ */
+double mouse_last_xpos = SCR_W / 2;
+double mouse_last_ypos = SCR_H / 2;
+bool mouse_first_capture = true;
+double camera_pitch = 0, camera_yaw = 0;
+float fov = 45;
+
+void framebuffer_size_callback(GLFWwindow *window, int w, int h) {
   glViewport(0, 0, w, h);
 }
 void process_input(GLFWwindow *window) {
   // Close window on Esc pressed
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     glfwSetWindowShouldClose(window, true);
+  float cam_speed = 2.5 * delta_time;
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    cam_pos += cam_speed * cam_front;
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    cam_pos -= cam_speed * cam_front;
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    cam_pos += cam_speed * glm::normalize(glm::cross(cam_up, cam_front));
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    cam_pos -= cam_speed * glm::normalize(glm::cross(cam_up, cam_front));
+  if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+    cam_pos += cam_speed * cam_up;
+  if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+    cam_pos -= cam_speed * cam_up;
+}
+void mouse_move_callback(GLFWwindow *window, double xpos, double ypos) {
+  if (mouse_first_capture) {
+    mouse_first_capture = false;
+    mouse_last_xpos = xpos;
+    mouse_last_ypos = ypos;
   }
+  double delta_xpos = xpos - mouse_last_xpos;
+  double delta_ypos = ypos - mouse_last_ypos;
+  delta_ypos = -delta_ypos;
+
+  mouse_last_xpos = xpos;
+  mouse_last_ypos = ypos;
+
+  float sensitivity = 0.05;
+  camera_pitch += sensitivity * delta_ypos;
+  camera_yaw += sensitivity * delta_xpos;
+
+  // Restrict the pitch range
+  camera_pitch = camera_pitch <= 89.0 ? camera_pitch : 89.0;
+  camera_pitch = camera_pitch >= -89.0 ? camera_pitch : -89.0;
+
+  glm::vec3 front;
+  front.x = cos(glm::radians(camera_pitch)) * sin(glm::radians(camera_yaw));
+  front.y = sin(glm::radians(camera_pitch));
+  front.z = -cos(glm::radians(camera_pitch)) * cos(glm::radians(camera_yaw));
+
+  cam_front = front;
+}
+
+void mouse_scroll_callback(GLFWwindow *window, double xoffset, double yoffset) {
+  fov-= yoffset;
+  fov = fov <= 45 ? fov : 45;
+  fov = fov >= 1 ? fov : 1;
 }
 
 int main() {
@@ -31,7 +98,6 @@ int main() {
   // set the OpenGL version
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);  // OpenGL 3.x
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);  // OpenGL x.3
-
   glfwWindowHint(GLFW_OPENGL_PROFILE,
                  GLFW_OPENGL_CORE_PROFILE);  // use core profile
   // Get the glfw window
@@ -43,6 +109,11 @@ int main() {
     return -1;
   }
   glfwMakeContextCurrent(window);
+  // Mouse input
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  glfwSetCursorPosCallback(window, mouse_move_callback);
+  glfwSetScrollCallback(window, mouse_scroll_callback);
+
   // use GLAD to manage OpenGL function pointers
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {  // check
     std::cout << "Failed to initialize GLAD" << std::endl;
@@ -52,7 +123,7 @@ int main() {
   // It's independent from window size of GLFW
   glViewport(0, 0, 800, 600);
   // Register the resize callback
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_cb);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   /**
    * @brief build shader program
@@ -164,6 +235,17 @@ int main() {
     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
   };
   glm::vec3 cube_pos[] = {
+    // glm::vec3( 0.0f,  0.0f,  0.0f),
+    // glm::vec3( 2.0f,  0.0f,  0.0f),
+    // glm::vec3( 4.0f,  0.0f,  0.0f),
+    // glm::vec3( 6.0f,  0.0f,  0.0f),
+    // glm::vec3( 8.0f,  0.0f,  0.0f),
+    // glm::vec3(10.0f,  0.0f,  0.0f),
+    // glm::vec3(12.0f,  0.0f,  0.0f),
+    // glm::vec3(14.0f,  0.0f,  0.0f),
+    // glm::vec3(16.0f,  0.0f,  0.0f),
+    // glm::vec3(18.0f,  0.0f,  0.0f)
+
     glm::vec3( 0.0f,  0.0f,  0.0f),
     glm::vec3( 2.0f,  5.0f, -15.0f),
     glm::vec3(-1.5f, -2.2f, -2.5f),
@@ -230,6 +312,10 @@ int main() {
 
     /****** Logic ******/
     glfwPollEvents();
+    float cur_frame = glfwGetTime();
+    delta_time = cur_frame - last_frame;
+    last_frame = cur_frame;
+    // std::cout << "delta time: " << delta_time << "\n";
 
     /****** Render ******/
     glm::mat4 view{1};
@@ -237,13 +323,12 @@ int main() {
     double radius = 10;
     double cam_x = sin(time) * radius;
     double cam_z = cos(time) * radius;
-    view = glm::lookAt(glm::vec3{cam_x, 0, cam_z}, glm::vec3{0, 0, 0},
-                       glm::vec3{0, 1, 0});
+    view = glm::lookAt(cam_pos, cam_pos + cam_front, cam_up);
 
     glm::mat4 proj{1};
     GLuint loc;
-    proj = glm::perspective(glm::radians(45.0f), float(1.0 * SCR_W / SCR_H),
-                            0.1f, 100.0f);
+    proj = glm::perspective(glm::radians(fov), float(1.0 * SCR_W / SCR_H), 0.1f,
+                            100.0f);
     loc = glGetUniformLocation(shader.ID, "view");
     glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(view));
 
