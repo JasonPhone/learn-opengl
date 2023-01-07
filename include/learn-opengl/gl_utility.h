d08418df8c4f55f74222ae4701110b6918c71c60
ja50n <zs_feng@qq.com>
2023-01-07 21:21:43 +0800

    added choosing warp type when loading texture

--- include/learn-opengl/gl_utility.h
+++ include/learn-opengl/gl_utility.h
@@ -53,32 +53,70 @@ GLenum glCheckError_(const char* file, int line) {
 #define glCheckError() glCheckError_(__FILE__, __LINE__)
 
 void LOG_(const char* file, int line, const char* prompt, const char* content) {
-  std::cerr << file << "(" << line << "): " << prompt  << " " << content << std::endl;
+  std::cerr << file << "(" << line << "): " << prompt << " " << content
+            << std::endl;
 }
 #define LOG(prompt, content) LOG_(__FILE__, __LINE__, prompt, content)
 
-unsigned int load_texture(char const *path) {
+/**
+ * @brief Load a texture from image using stb_image.
+ *
+ * @param path Path to image.
+ * @param warp_type S and T warp type. `GL_REPEAT`, `GL_MIRRORED_REPEAT`,
+ * `CLAMP_TO_EDGE` and `GL_CLAMP_TO_BORDER`. `GL_REPEAT` by default.
+ * @return unsigned int ID of texture.
+ */
+unsigned int load_texture(char const* path, GLenum warp_type = GL_REPEAT) {
   unsigned int textureID;
   glGenTextures(1, &textureID);
 
   int width, height, nrComponents;
-  unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+  unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
   if (data) {
-    GLenum format;
-    if (nrComponents == 1)
-      format = GL_RED;
-    else if (nrComponents == 3)
-      format = GL_RGB;
-    else if (nrComponents == 4)
-      format = GL_RGBA;
+    GLenum format = GL_RED;
+    switch (nrComponents) {
+      case 1: {
+        format = GL_RED;
+        break;
+      }
+      case 3: {
+        format = GL_RGB;
+        break;
+      }
+      case 4: {
+        format = GL_RGBA;
+        break;
+      }
+    }
+    // For sanity check.
+    GLenum warp = GL_REPEAT;
+    switch (warp_type) {
+      case GL_REPEAT: {
+        warp = GL_REPEAT;
+        break;
+      }
+      case GL_MIRRORED_REPEAT: {
+        warp = GL_MIRRORED_REPEAT;
+        break;
+      }
+      case GL_CLAMP_TO_EDGE: {
+        warp = GL_CLAMP_TO_EDGE;
+        break;
+      }
+      case GL_CLAMP_TO_BORDER: {
+        warp = GL_CLAMP_TO_BORDER;
+        break;
+      }
+    }
 
     glBindTexture(GL_TEXTURE_2D, textureID);
     glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format,
                  GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, warp);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, warp);
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
                     GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
